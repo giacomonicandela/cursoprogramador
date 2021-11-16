001de5b02fbddc36c177a34034c3d159ae8234c5
Candela Giacomoni <giacomonicandelar@2gmail.com>
2021-11-16 17:41:00 -0300

    imagenes

--- modulo5/CRUD/app.js
+++ modulo5/CRUD/app.js
@@ -6,11 +6,13 @@ var logger = require('morgan');
 
 require('dotenv').config();
 var session = require('express-session');
+var fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
+var conocenosRouter = require('./routes/conocenos');
 
 var app = express();
 
@@ -43,10 +45,16 @@ secured = async (req, res, next) => {
   }
 }
 
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
+app.use('/conocenos', conocenosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- modulo5/CRUD/routes/conocenos.js
+++ modulo5/CRUD/routes/conocenos.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
-var novedadesModel = require('./../models/novedadesModel');
-var cloudinary = require('cloudinary').v2;
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-
-  var novedades = await novedadesModel.getNovedades();
-  novedades = novedades.splice(0, 5);
-  novedades = novedades.map(novedad => {
-    if (novedad.img_id) {
-      const imagen = cloudinary.url(novedad.img_id, {
-        width: 460,
-        crop: 'fill'
-      });
-      return {
-        ...novedad,
-        imagen
-      }
-    } else {
-      return {
-       ...novedad,
-       imagen: '/images/noimage.jpg'
-      }
-    }
-  });
-
-  res.render('index',{
-    novedades
-  });
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('conocenos', { title: 'Express'});
 });
 
 router.post('/', async (req, res, next) => {
@@ -59,11 +34,10 @@ router.post('/', async (req, res, next) => {
 
   var info = await transporter.sendMail(obj);
 
-  res.render('index', {
+  res.render('conocenos', {
     message: 'Mensaje enviado correctamente',
   });
 
 })
 
 module.exports = router;
-
